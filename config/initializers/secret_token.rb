628e31ca3449109ead98c586256bcc28795477b0
leemour <leemour@gmail.com>
2013-07-24 12:08:29 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awd3::Application.config.secret_token = '016e1ffcd100c7c35069930b59c394ea4a4a174b62fd26f4499f713be81d4ca76c5fa2aeaea537c2cc62e54723ef245d7784ec633dc9d4bc7f292760cc1dc43e'
